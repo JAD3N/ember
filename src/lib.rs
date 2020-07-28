a1e2eef5e1f53b10e7aedd53615d9f00488bb7eb
JAD3N <me@jaden.bio>
2020-07-29 00:54:28 +0100

    Refactor App

--- src/lib.rs
+++ src/lib.rs
@@ -3,46 +3,118 @@ extern crate log;
 
 pub mod app;
 
+use app::App;
 use winit::{
-    event::Event,
+    event::{Event, WindowEvent},
     event_loop::EventLoop,
     window::Window,
+    dpi::PhysicalSize,
 };
 
-fn run(window: Window) {
+struct Setup {
+    window: Window,
+    event_loop: EventLoop<()>,
+    instance: wgpu::Instance,
+    size: PhysicalSize<u32>,
 
+    surface: wgpu::Surface,
+    adapter: wgpu::Adapter,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
 }
 
-pub fn init() {
-    // let event_loop = EventLoop::new();
-    // let window = Window::new(&event_loop).unwrap();
+async fn setup<A: App>() -> Setup {
+    #[cfg(feature = "subscriber")]
+    {
+        let chrome_tracing_dir = std::env::var("WGPU_CHROME_TRACE");
+        wgpu::util::initialize_default_subscriber(
+            chrome_tracing_dir.as_ref().map(std::path::Path::new).ok(),
+        );
+    };
 
-    // #[cfg(not(target_arch = "wasm32"))]
-    // {
-    //     #[cfg(feature = "subscriber")]
-    //     wgpu::util::initialize_default_subscriber(None);
+    #[cfg(target_arch = "wasm32")]
+    {
+        console_error_panic_hook::set_once();
+        console_log::init().expect("couldn't initialize logger");
+    };
 
-    //     futures::executor::block_on(async {
-    //         info!("");
-    //         loop {}
-    //     });
-    // }
+    let event_loop = EventLoop::new();
+    let window = winit::window::WindowBuilder::new()
+        .with_title(A::title())
+        .build(&event_loop)
+        .unwrap();
 
-    // #[cfg(target_arch = "wasm32")]
-    // {
-    //     console_error_panic_hook::set_once();
-    //     console_log::init().expect("couldn't initialize logger");
+    let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+    let (size, surface) = unsafe {
+        let size = window.inner_size();
+        let surface = instance.create_surface(&window);
 
-    //     use winit::platform::web::WindowExtWebSys;
+        (size, surface)
+    };
 
-    //     web_sys::window()
-    //         .and_then(|win| win.document())
-    //         .and_then(|document| document.body())
-    //         .and_then(|body| body.append_child(&web_sys::Element::from(window.canvas())).ok())
-    //         .expect("couldn't create canvas");
+    let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions {
+        power_preference: wgpu::PowerPreference::HighPerformance,
+        compatible_surface: Some(&surface),
+    }).await.unwrap();
 
-    //     wasm_bindgen_futures::spawn_local(async {
-    //         info!("wasm test");
-    //     })
-    // }
+    let trace_dir = std::env::var("WGPU_TRACE");
+    let (device, queue) = adapter.request_device(
+        &wgpu::DeviceDescriptor {
+            features: wgpu::Features::empty(),
+            limits: wgpu::Limits::default(),
+            shader_validation: true,
+        },
+        trace_dir.ok()
+            .as_ref()
+            .map(std::path::Path::new),
+    ).await.unwrap();
+
+    Setup {
+        window,
+        event_loop,
+        instance,
+        size,
+        surface,
+        adapter,
+        device,
+        queue,
+    }
+}
+
+fn start(
+    Setup {
+        window,
+        event_loop,
+        instance,
+        size,
+        surface,
+        adapter,
+        device,
+        queue,
+    }: Setup,
+) {
+    event_loop.run(move |event, _, control_flow| {
+        if let Event::WindowEvent { event, .. } = event {
+            match event {
+                WindowEvent::CloseRequested => println!("close me!"),
+                _ => {},
+            }
+        }
+    });
+}
+
+pub fn run<A: App>() {
+    #[cfg(target_arch = "wasm32")]
+    {
+        wasm_bindgen_futures::spawn_local(async move {
+            let setup = setup::<A>().await;
+            start(setup);
+        });
+    };
+
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        let setup = futures::executor::block_on(setup::<A>());
+        start(setup);
+    };
 }
